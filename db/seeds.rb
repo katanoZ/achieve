fead74cb7686d291154dbf445e8765f819491a6b
Katanosaka Hiroaki <katano9@yahoo.co.jp>
2017-04-27 12:52:15 +0000

    complete DIVE09

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+100.times do |n|
+    name = Faker::Name.name
+    email = Faker::Internet.email
+    password = "password"
+    User.create!(email: email,
+        name: name,
+        password: password,
+        password_confirmation: password,
+        )
+end
+
+
+n = 1
+while n <= 100
+    title = Faker::Book.title
+    content = Faker::Hacker.say_something_smart
+    Blog.create(
+        title: title,
+        content: content,
+        user_id: n,
+        )
+    n = n + 1
+end
