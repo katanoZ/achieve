2f94aeb65ae94c3d075f9c6eb6e008dee0977aea
Katanosaka Hiroaki <katano9@yahoo.co.jp>
2017-05-07 21:16:18 +0900

    fix mailer function in blogs

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,13 +79,12 @@ Rails.application.configure do
 
   config.aciton_mailer.default_url_options = { host: "calm-dusk-90631.herokuapp.com" }
   AcitonMailer::Base.delivery_method = :smtp
-  AcitonMailer::Base.smtp_settings =
-  {
-    user_name: ENV["SENDGRID_USERNAME"],
-    password: ENV["SENDGRID_PASSWORD"],
+  config.action_mailer.smtp_settings = {
+    user_name: ENV['SENDGRID_USERNAME'],
+    password: ENV['SENDGRID_PASSWORD'],
     domain: "heroku.com",
-    address: "smtp.sendgrid.net",
-    port: 587,
+    address:"smtp.sendgrid.net",
+    port:587,
     authentication: :plain,
     enable_starttls_auto: true
   }
