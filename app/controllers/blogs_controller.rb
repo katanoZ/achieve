753980005da1048f763e220b4294df15764febe5
片ノ坂 浩明 <katano9@yahoo.co.jp>
2017-05-07 20:46:54 +0900

    add mailer function to blogs

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -19,14 +19,15 @@ class BlogsController < ApplicationController
     @blog.user_id = current_user.id
     if @blog.save
       redirect_to blogs_path, notice: "ブログを作成しました！"
+      NoticeMailer.sendmail_blog(@blog).deliver
     else
       render "new"
     end
   end
-  
+
   def edit
   end
-  
+
   def update
     if @blog.update(blogs_params)
       redirect_to blogs_path, notice: "ブログを更新しました！"
@@ -34,22 +35,22 @@ class BlogsController < ApplicationController
       render "edit"
     end
   end
-  
+
   def confirm
     @blog = Blog.new(blogs_params)
     render :new if @blog.invalid?
   end
-  
+
   def destroy
     @blog.destroy
     redirect_to blogs_path, notice: "ブログを削除しました！"
   end
-  
+
   private
   def blogs_params
     params.require(:blog).permit(:title, :content)
   end
-  
+
   #idをキーとして値を取得
   def set_blog
     @blog = Blog.find(params[:id])
