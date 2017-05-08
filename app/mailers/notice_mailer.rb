d00a7e0d2f9d0b366b416becc89ae4421026515e
Katanosaka Hiroaki <katano9@yahoo.co.jp>
2017-05-08 20:42:41 +0900

    after assignment DIVE11

--- app/mailers/notice_mailer.rb
+++ app/mailers/notice_mailer.rb
@@ -11,4 +11,12 @@ class NoticeMailer < ApplicationMailer
     mail to: "katano9@yahoo.co.jp",
       subject: "【Achieve】ブログが投稿されました"
   end
+
+  def sendmail_contact(contact)
+    @contact = contact
+
+    mail to: @contact.email,
+      subject: "【Achieve】お問い合わせありがとうございました"
+  end
+
 end
