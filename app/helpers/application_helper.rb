3bfe08d16fa3140cd318ef77043734ca974cdf63
Katanosaka Hiroaki <katano9@yahoo.co.jp>
2017-07-20 20:27:34 +0900

    [DIVE19_2][update]通知一覧ページをデザイン

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,12 @@
 module ApplicationHelper
-  def profile_img(user, size = "")
+  def profile_img(user, size: :default)
 
     if user.avatar?
       case size
-      when "small"
+      when :small
         return image_tag(user.avatar, alt: user.name, size: "100x100", class: "img-circle")
+      when :xsmall
+        return image_tag(user.avatar, alt: user.name, size: "50x50", class: "img-circle")
       else
         return image_tag(user.avatar, alt: user.name)
       end
@@ -17,8 +19,10 @@ module ApplicationHelper
     end
 
     case size
-    when "small"
+    when :small
       image_tag(image_url, alt: user.name, size: "100x100", class: "img-circle")
+    when :xsmall
+      image_tag(image_url, alt: user.name, size: "50x50", class: "img-circle")
     else
       image_tag(image_url, alt: user.name)
     end
