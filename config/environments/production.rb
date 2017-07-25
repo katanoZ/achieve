8991d067c1efcdf1bb33a5012a9d02b7f97d6b6d
Katanosaka Hiroaki <katano9@yahoo.co.jp>
2017-07-26 00:02:39 +0900

    [DIVE20_1][fix]SendGrid設定をAWS用に修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,13 +77,13 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: "calm-dusk-90631.herokuapp.com" }
+  config.action_mailer.default_url_options = { host: "13.113.230.251" }
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings =
   {
    user_name: ENV['SENDGRID_USERNAME'],
    password: ENV['SENDGRID_PASSWORD'],
-   domain: "heroku.com",
+#   domain: "heroku.com",
    address: "smtp.sendgrid.net",
    port: 587,
    authentication: :plain,
